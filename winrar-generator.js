95c06de14b5e7e54f218176df7e7679d088fbbe0
Amiral Ariska <ariskaamiral@gmail.com>
2025-12-10 14:40:35 +0800

    Enhance user prompts and add archive format selection for folder and file actions

--- winrar-generator.js
+++ winrar-generator.js
@@ -7,45 +7,84 @@ var fs = require('fs');
 var path = require('path');
 var inquirer = require('inquirer');
 
-var version = program.version(`${chalk.hex('#00f')('0.0.4')}
-${gradient('#f0f', '#00f', '#0f0')('Winrar Generator')}`, '-v, --version', 'display the version');
+var version = program.version(`${chalk.hex('#00f')('0.0.5')}
+${gradient('#f0f', '#00f', '#0f0')('Winrar Generator Tool')}`, '-v, --version', 'display the version');
 program.parse(process.argv);
-console.log(gradient('#f0f', '#00f', '#0f0')('Welcome to Winrar Generator'));
+console.log(gradient('#f0f', '#00f', '#0f0')('Welcome to Winrar Generator Tool!'));
+
 var winrargenerator = inquirer.prompt([{
-	name: 'extractorcompress',
-	message: 'what action do you want to perform',
-	type: 'list',
-	choices: ["Extract", "Archive folder", "Archive file"]
+    name: 'extractorcompress',
+    message: 'What action do you want to perform?',
+    type: 'list',
+    choices: ["Extract", "Archive folder", "Archive file"]
 }]).then(({ extractorcompress }) => {
-	var contents = fs.readdirSync(".");
-	inquirer.prompt([{
-		name: 'inputFilename',
-		message: `Which file do you want to ${extractorcompress}`,
-		type: 'list',
-		choices: contents,
-	}]).then(({ inputFilename }) => {
-		if (extractorcompress === "Extract") extract(inputFilename);
-		if (extractorcompress === "Archive folder") archivefolder(inputFilename);
-		if (extractorcompress === "Archive file") archivefile(inputFilename);
-	})
-})
-function extract(filename){
-	const {name} = path.parse(filename);
-	const OutputFilename = `${name}`;
-	const zip = new AdmZip(`./${filename}`);
-	zip.extractAllTo(`./${OutputFilename}`);
+    var contents = fs.readdirSync(".");
+    inquirer.prompt([{
+        name: 'inputFilename',
+        message: `Which file or folder do you want to ${extractorcompress}?`,
+        type: 'list',
+        choices: contents,
+    }]).then(({ inputFilename }) => {
+        if (extractorcompress === "Extract") {
+            extract(inputFilename);
+        } else {
+            inquirer.prompt([{
+                name: 'archiveFormat',
+                message: 'Which archive format do you want to use?',
+                type: 'list',
+                choices: [".zip", ".rar"]
+            }]).then(({ archiveFormat }) => {
+                if (extractorcompress === "Archive folder") {
+                    archivefolder(inputFilename, archiveFormat);
+                }
+                if (extractorcompress === "Archive file") {
+                    archivefile(inputFilename, archiveFormat);
+                }
+            });
+        }
+    });
+});
+
+function extract(filename) {
+    const { name } = path.parse(filename);
+    const OutputFilename = `${name}`;
+    const zip = new AdmZip(`./${filename}`);
+    zip.extractAllTo(`./${OutputFilename}`);
+    console.log(chalk.green(`Extracted ${filename} to ${OutputFilename}`));
 }
-function archivefolder(filename){
-	const {name} = path.parse(filename);
-	const OutputFilename = `${name}.rar`;
-	const zip = new AdmZip();
-	zip.addLocalFolder(`./${filename}`);
-	zip.writeZip(`./${OutputFilename}`);
+
+function archivefolder(foldername, format) {
+    const folderPath = `./${foldername}`;
+    if (!fs.lstatSync(folderPath).isDirectory()) {
+        console.log(chalk.red(`${foldername} is not a folder.`));
+        return;
+    }
+
+    const { name } = path.parse(foldername);
+    const OutputFilename = `${name}${format}`;
+    const zip = new AdmZip();
+
+    // Add the folder's contents to the archive
+    zip.addLocalFolder(folderPath, name); // The second argument ensures the folder structure is preserved
+    zip.writeZip(`./${OutputFilename}`);
+
+    console.log(chalk.green(`Archived folder ${foldername} to ${OutputFilename}`));
 }
-function archivefile(filename){
-	const {name} = path.parse(filename);
-	const OutputFilename = `${name}.rar`;
-	const zip = new AdmZip();
-	zip.addLocalFile(`./${filename}`);
-	zip.writeZip(`./${OutputFilename}`);
+
+function archivefile(filename, format) {
+    const filePath = `./${filename}`;
+    if (!fs.lstatSync(filePath).isFile()) {
+        console.log(chalk.red(`${filename} is not a file.`));
+        return;
+    }
+
+    const { name } = path.parse(filename);
+    const OutputFilename = `${name}${format}`;
+    const zip = new AdmZip();
+
+    // Add the file to the archive
+    zip.addLocalFile(filePath);
+    zip.writeZip(`./${OutputFilename}`);
+
+    console.log(chalk.green(`Archived file ${filename} to ${OutputFilename}`));
 }
